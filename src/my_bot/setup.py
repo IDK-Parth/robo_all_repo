388430331e2afb585674b9c3c6e898544e98e86d
Tanvi Ahire <tanvipahire04@gmail.com>
2026-06-16 12:59:35 +0530

    Working SLAM and map generation state

--- src/my_bot/setup.py
+++ src/my_bot/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-from glob import glob
 import os
+from glob import glob
 
 package_name = 'my_bot'
 
@@ -8,28 +8,25 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-data_files=[
-    ('share/ament_index/resource_index/packages',
-        ['resource/my_bot']),
-    ('share/my_bot', ['package.xml']),
-
-    
-    ('share/my_bot/launch', ['launch/display.launch.py']),
-
-   
-    ('share/my_bot/urdf', ['urdf/my_bot.urdf']),
-],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/worlds', glob('worlds/*')),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='mansha',
-    maintainer_email='mansha@example.com',
-    description='Delivery bot simulation package',
+    maintainer='tanvi',
+    maintainer_email='tanvi@example.com',
+    description='Delivery bot',
     license='Apache License 2.0',
-    tests_require=['pytest'],
-  entry_points={
-    'console_scripts': [
-        'move_robot = my_bot.move_robot:main',
-        'imu_odom = my_bot.imu_odom:main',
-    ],
-},
+    entry_points={
+        'console_scripts': [
+            'move_robot = my_bot.move_robot:main',
+            'imu_odom = my_bot.imu_odom:main',
+            'multi_goal_nav = my_bot.multi_goal_nav:main',
+        ],
+    },
 )
